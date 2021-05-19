1aaf4895d84a27f66089385c1700afb82fc713ee
EricJL1994 <eric.jl1994@gmail.com>
2021-05-20 00:19:54 +0100

    Add from post app

--- src/controllers/problemController.js
+++ src/controllers/problemController.js
@@ -19,13 +19,11 @@ exports.problem_show = async function(req, res) {
   console.log('Request petition:')
   console.log(req.query)
 
-  const resultTest = await Boulder.find().sort({dificultyName: 'asc', number: 'asc'})
+  const mongoBoulders = await Boulder.find().sort({dificultyName: 'asc', number: 'asc'})
                       .then(result => result.map(boulder => {
                           return {...boulder.toObject(), date: formatDate(convertTicksToDate(boulder.dateValue))}
                         })
                       ).catch(err => console.log(err))
-
-  console.log(resultTest)
 
   // const resultBoulders = database.get(problemTypesToJSONDatabase['Boulder']).sortBy(['dificultyName', 'number']).value().map(boulder => {
   //   return {...boulder, date: formatDate(convertTicksToDate(boulder.dateValue))}
@@ -35,20 +33,7 @@ exports.problem_show = async function(req, res) {
     return {...traverse, date: formatDate(convertTicksToDate(traverse.dateValue))}
   })
 
-  res.render("show_problems", {arrayBoulders: resultTest,  arrayTraverses: resultTraverses})
-
-  /*const result = database.get(
-    problemTypesToJSONDatabase['Boulder'], problemTypesToJSONDatabase['Traverse']).value().map(
-      element => `${element.dificultyName}_${element.number} - ${formatDate(convertTicksToDate(element.dateValue))}`)
-
-  var html = ""
-  result.forEach(element => {
-    html+= htmlEnclose(element, 'li')
-  })
-
-  html = htmlEnclose(html, 'ul')
-  //html = htmlEnclose(html, 'table')
-  res.send(html)*/
+  res.render("show_problems", {arrayBoulders: mongoBoulders,  arrayTraverses: resultTraverses})
 }
 
 exports.problem_detail = function(req, res) {
@@ -95,29 +80,43 @@ exports.problem_add = function(req, res) {
   }
 }
 
-exports.problem_add_multiple = function(req, res) {
+exports.test = async function(req, res){
+  console.log("TEST")
+  const mongoExists = await Boulder.findOne({dificultyName: "Green", number: 1}).exec()
+
+  console.log(mongoExists);
+  res.send();
+}
+
+exports.problem_add_multiple = async function(req, res) {
   const newProblemsData = JSON.parse(req.query.problems)
   const problemsType = req.query.type
   const databaseSize = database.get(problemsType).size().value()
   var edited = false
-  newProblemsData.map(problemString => {
+
+  newProblemsData.map(async problemString=> {
     const problem = JSON.parse(problemString)
-    const problemExists = database.get(problemsType).find({ dificultyName: problem.dificultyName, number: problem.number }).value()
-    
-    if (!problemExists) {
+    //const problemExists = database.get(problemsType).find({ dificultyName: problem.dificultyName, number: problem.number }).value()
+    //const mongoExists = Boulder.findOne({dificultyName: problem.dificultyName, number: problem.number}).exec()
+
+    const mongoExists = await Boulder.findOne({dificultyName: problem.dificultyName, number: problem.number}).exec()
+
+    if (!mongoExists) {
       console.log(problem)
-      database.get(problemsType).push(problem).write()
+      Boulder.create(problem)
+      //database.get(problemsType).push(problem).write()
   
       logger('SUCCESS', 'Problema creado')
     } else {
       edited = true
-      database.get(problemsType).find(problemExists).assign({
+      /*database.get(problemsType).find(problemExists).assign({
         "dateValue": problem.dateValue,
         "holdColor": problem.holdColor,
         "pending": problem.pending,
         "intersectionsName": problem.intersectionsName,
         "wall": problem.wall
-      }).write();
+      }).write();*/
+      Boulder.findOneAndReplace({dificultyName: problem.dificultyName, number: problem.number}, problem).exec()
       logger('SUCCESS', 'Problema editado')
     }
   })
